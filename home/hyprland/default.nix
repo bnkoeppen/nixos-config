7fe16501d98cdec37cd9bb78e7ecad4e62ec6cad
bnkoeppen <bnkoeppen@gmail.com>
2026-02-28 18:46:38 -0500

    change hyprland active color

--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -28,8 +28,7 @@
       gaps_out = 10;
       border_size = 2;
 
-      # "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
-      "col.active_border" = "rgba(33ccffee)";
+      "col.active_border" = "rgba(148fb8ee)";
       "col.inactive_border" = "rgba(595959aa)";
       resize_on_border = false;
       allow_tearing = false;
