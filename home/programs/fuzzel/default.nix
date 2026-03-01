1826fa3d36c43e9ae323eb972cbc033a625b74e5
bnkoeppen <bnkoeppen@gmail.com>
2026-02-28 19:30:18 -0500

    fuzzel color scheme

--- home/programs/fuzzel/default.nix
+++ home/programs/fuzzel/default.nix
@@ -2,5 +2,14 @@
 {
   programs.fuzzel = {
     enable = true;
+    settings = {
+      colors.background = "222222cc";
+      colors.border = "148fb8ee";
+      colors.text = "eeeeeeff";
+      colors.selection-text = "148fb8ee";
+      colors.selection = "00000000";
+
+      border.width = 4;
+    };
   };
 }
