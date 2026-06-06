3df018d68972efe5d0982291171a1805aced1ace
bnkoeppen <bnkoeppen@gmail.com>
2026-06-06 14:10:40 -0600

    basic neovim setup 2

--- homeManagerModules/common/programs/neovim.nix
+++ homeManagerModules/common/programs/neovim.nix
@@ -3,5 +3,14 @@
   programs.neovim = {
     enable = true;
     defaultEditor = true;
+
+    extraConfig = ''
+      set relativenumber
+      set list
+    '';
+
+    # Post 25.05 behaviors
+    withRuby = false;
+    withPython3 = true;
   };
 }
