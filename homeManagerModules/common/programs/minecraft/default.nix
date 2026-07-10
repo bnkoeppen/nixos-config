3f0a29ef4f5b7ee4eeff6488a4426f4f1e69f4a9
bnkoeppen <bnkoeppen@gmail.com>
2026-07-10 17:03:16 -0600

    cleanroom compat

--- homeManagerModules/common/programs/minecraft/default.nix
+++ homeManagerModules/common/programs/minecraft/default.nix
@@ -1,19 +1,6 @@
-{ pkgs, ... }:
+{ ... }:
 {
   programs.prismlauncher = {
     enable = true;
-
-    package = pkgs.prismlauncher.override (old: {
-      jdks = (
-        if old ? jdks then
-          old.jdks
-        else
-          [
-            pkgs.temurin-bin-8
-            pkgs.temurin-bin-17
-            pkgs.temurin-bin-21
-          ]
-      );
-    });
   };
 }
